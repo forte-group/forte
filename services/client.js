2caf7743d7b29d17d6f5035013d74a9bc417bbbe
Dillon Brock <mdillon1297@gmail.com>
2022-07-16 16:10:51 -0700

    groundwork for leaderboard component

--- services/client.js
+++ services/client.js
@@ -3,3 +3,13 @@ const SUPABASE_KEY =
 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6a25rdGpyYnVneHRxemxvbWR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTc5MjE5NzQsImV4cCI6MTk3MzQ5Nzk3NH0.GR3I5xx6bOg0tY0fcM1yE3LYix_FTXVA6F1lRBqPhYU';
 
 export const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export function checkResponse({ error, data }) {
+    if (error) {
+        // eslint-disable-next-line no-console
+        console.log(error);
+        return null;
+    }
+
+    return data;
+}
