2eabdc0278ad9cb8c144db6c195a191b973ced17
Jmart5564 <Jmart5564@gmail.com>
2022-07-18 10:46:57 -0700

    added grid function

--- app.js
+++ app.js
@@ -4,18 +4,46 @@ import createUser from './components/User.js';
 
 // State
 let user = null;
+let sequenceLength = 8;
+let numberOfGuesses = 4;
 
 // Action Handlers
 async function handlePageLoad() {
     user = getUser();
     protectPage(user);
-
+    
     display();
 }
 
 async function handleSignOut() {
     signOut();
 }
+
+
+let fragment = document.createDocumentFragment();
+
+let generateGameGrid = () => {
+    
+    let grid = document.querySelector('#game-grid');
+    grid.innerHTML = '';
+
+    Array.from({ length: numberOfGuesses }).forEach(() => {
+        let row = document.createElement('div');
+        row.classList.add('row');
+        
+        Array.from({ length: sequenceLength }).forEach(() => {
+            let tile = document.createElement('div');
+            tile.classList.add('tile');
+            
+            row.appendChild(tile);
+        });
+        
+        fragment.appendChild(row);
+    });
+
+    grid.appendChild(fragment);
+
+};
 
 // Components 
 const User = createUser(
@@ -25,6 +53,7 @@ const User = createUser(
 
 function display() {
     User({ user });
+    generateGameGrid();
 
 }
 
