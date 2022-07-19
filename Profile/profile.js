b838ca49eda2759e9d11216ebe50bc14c4a7b415
Hathaway-Shan <hathaway.shan@gmail.com>
2022-07-19 16:54:40 -0700

    fixed issue with avatar being blank if user had not selected an image

--- Profile/profile.js
+++ Profile/profile.js
@@ -3,6 +3,7 @@ import { getProfile, getUser, signOut, updateProfile, uploadAvatar } from '../se
 import createUser from '../components/User.js';
 import createUpsertProfile from '../components/upsertProfile.js';
 
+
 //state
 let user = null;
 let profile = null;
@@ -11,7 +12,12 @@ let profile = null;
 async function handlePageLoad() {
     user = getUser();
 
+
     profile = await getProfile();
+    if (profile.avatar_url === null) {
+        profile.avatar_url = 'https://vzknktjrbugxtqzlomdz.supabase.co/storage/v1/object/public/avatars/f7d0a9e4-b59d-41a4-8f0b-a1ea8286f40c/musician-removebg-preview.png';
+    }
+
     display();
 }
 
